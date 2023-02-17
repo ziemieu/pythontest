3e1dde0c7b2d511f26061ff819117f5179ed05e9
ziemieu <63301604+ziemieu@users.noreply.github.com>
2023-02-17 15:51:42 +0000

    Adding test with Features

--- tests/test_greeting.py
+++ tests/test_greeting.py
@@ -1,4 +1,21 @@
+import pytest
 from greeting import my_name
 
-def test_my_name():
-    assert "My name is: Mary" == my_name("Mary")
+##### Testing Features
+@pytest.fixture
+def mary():
+    return "My name is: Mary"
+
+@pytest.fixture
+def john():
+    return "My name is: John"
+
+def test_mary(mary):
+    assert "Mary" == my_name("Mary")
+
+def test_john(john):
+    assert "John" == my_name("Mary")
+
+##### This is for testing module
+# def test_my_name():
+#     assert "My name is: Mary" == my_name("Mary")
